be7eb99efb14c0b10081b135c7b625f2531e7c4a
patrik127 <patrik.purgar919@gmail.com>
2017-12-01 16:24:31 +0200

    link email issue is fixed

--- imports/ui/components/Footer/Footer.jsx
+++ imports/ui/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { translate } from 'react-i18next'
-import { Grid, List, Icon, Button } from 'semantic-ui-react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { translate } from 'react-i18next';
+import { Grid, List, Icon, Button } from 'semantic-ui-react';
 import {
   ShareButtons,
   ShareCounts,
@@ -52,51 +52,51 @@ const title = 'GitHub';
 
 const Footer = ({ t, isMobile }) => (
   <div className={isMobile ? 'ui segment inverted footer mobile-footer' : 'ui segment inverted footer'}>
-    <Grid className='page' textAlign='center'>
-      <Grid.Row className='pages-links'>
+    <Grid className="page" textAlign="center">
+      <Grid.Row className="pages-links">
         <Grid.Column width={2}>
           <List link inverted>
-            <List.Item><Link to='/'>{t('common:footer.home')}</Link></List.Item>
-            <List.Item><Link to='/'>{t('common:footer.aboutUs')}</Link></List.Item>
-            <List.Item><Link to='/'>{t('common:footer.play')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.home')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.aboutUs')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.play')}</Link></List.Item>
           </List>
         </Grid.Column>
         <Grid.Column width={2}>
           <List link inverted>
-            <List.Item><Link to='/'>{t('common:footer.faq')}</Link></List.Item>
-            <List.Item><a href='mailto:info@ryfma.ru'>{t('common:footer.casinoGame')}</a></List.Item>
-            <List.Item><Link to='/adv'>{t('common:footer.blog')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.faq')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.casinoGame')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.blog')}</Link></List.Item>
           </List>
         </Grid.Column>
         <Grid.Column width={3}>
           <List link inverted>
-            <List.Item><Link to='/'>{t('common:footer.workatCasino')}</Link></List.Item>
-            <List.Item><a href='mailto:info@ryfma.ru'>{t('common:footer.affiliates')}</a></List.Item>
-            <List.Item><Link to='/adv'>{t('common:footer.privacy')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.workatCasino')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.affiliates')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.privacy')}</Link></List.Item>
           </List>
         </Grid.Column>
         <Grid.Column width={3}>
           <List link inverted>
-            <List.Item><Link to='/'>{t('common:footer.contact')}</Link></List.Item>
-            <List.Item><a href='mailto:info@ryfma.ru'>{t('common:footer.termsConditions')}</a></List.Item>
+            <List.Item><Link to="/">{t('common:footer.contact')}</Link></List.Item>
+            <List.Item><Link to="/">{t('common:footer.termsConditions')}</Link></List.Item>
           </List>
         </Grid.Column>
         <Grid.Column width={4} className="flag-section">
-          <Button><img src="./images/contact.png" /></Button>
-          <img src="./images/russia.png" className="image-padding"/>
-          <img src="./images/japan.png" className="image-padding"/>
-          <img src="./images/france.png" className="image-padding"/>
-          <img src="./images/turkey.png" className="image-padding"/>
-          <img src="./images/uk.png" className="image-padding"/>
+          <Button><img src="./images/contact.png" alt="country" /></Button>
+          <img src="./images/russia.png" className="image-padding" alt="country" />
+          <img src="./images/japan.png" className="image-padding" alt="country" />
+          <img src="./images/france.png" className="image-padding" alt="country" />
+          <img src="./images/turkey.png" className="image-padding" alt="country" />
+          <img src="./images/uk.png" className="image-padding" alt="country" />
         </Grid.Column>
       </Grid.Row>
-      <Grid.Row textAlign='center' className="social-section">
+      <Grid.Row textAlign="center" className="social-section">
         <Grid.Column width={7} className="gambling">
-          <img src="./images/gam-image.png"/>
-          <img src="./images/begamble.png"/>
-          <img src="./images/gambling.png"/>
+          <img src="./images/gam-image.png" alt="country" />
+          <img src="./images/begamble.png" alt="country" />
+          <img src="./images/gambling.png" alt="country" />
         </Grid.Column>
-        <Grid.Column width={7} textAlign='right' className="btn-social">
+        <Grid.Column width={7} textAlign="right" className="btn-social">
           <div className="btn-social-groups">
             <FacebookShareButton
               url={shareUrl}
@@ -137,24 +137,24 @@ const Footer = ({ t, isMobile }) => (
               <VKIcon
                 size={45}
                 round />
-            </VKShareButton>  
+            </VKShareButton>
           </div>
         </Grid.Column>
       </Grid.Row>
-      <Grid.Row className='menu-copyright'>
-        <Grid.Column width={4} className='copyright' textAlign='center'>
-          <img src="./images/footer-logo.png"/>
+      <Grid.Row className="menu-copyright">
+        <Grid.Column width={4} className="copyright" textAlign="center">
+          <img src="./images/footer-logo.png" alt="country" />
         </Grid.Column>
-        <Grid.Column width={12} textAlign='right'>
+        <Grid.Column width={12} textAlign="right">
           <p>
-            CASINOFF is a company incorporated under the Laws of United Kingdom and Wales with company registration no. C55663, and having its registered address at ‘The Unicorn Centre, Triq l-Uqija, Swieqi, SWQ 2335, London'. <br/><br/>
-            Casinoff is licensed and regulated in the United Kingdom. This website provides facilities for gambling to persons in Great Britain in reliance to Gambling Commission Licence Number 000-039265-R-319417-009. Find out more about the UKGC on <a href="http://www.gamblingcommission.gov.uk" target="_blank">www.gamblingcommission.gov.uk</a> <br/><br/>
+            CASINOFF is a company incorporated under the Laws of United Kingdom and Wales with company registration no. C55663, and having its registered address at ‘The Unicorn Centre, Triq l-Uqija, Swieqi, SWQ 2335, London'. <br /><br />
+            Casinoff is licensed and regulated in the United Kingdom. This website provides facilities for gambling to persons in Great Britain in reliance to Gambling Commission Licence Number 000-039265-R-319417-009. Find out more about the UKGC on <a href="http://www.gamblingcommission.gov.uk" target="_blank">www.gamblingcommission.gov.uk</a> <br /><br />
             Games provided by IGT (Alderney 4) Limited and by Jadestone Networks (Malta) Limited are licensed and regulated by the Alderney Gambling Control Commission (AGCC). You may always contact us on <a href="#">hey@casumo.com</a>.
           </p>
         </Grid.Column>
       </Grid.Row>
     </Grid>
   </div>
-)
+);
 
-export default translate()(Footer)
+export default translate()(Footer);
